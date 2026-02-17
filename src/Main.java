b287b6fd043687f2102c38fe1e6c56e0f995e50b
Marqed <Derpexsplosion@gmail.com>
2026-02-17 11:48:42 -0500

    uneasy emoji

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.scene.paint.Color;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,14 +13,13 @@ public class Main extends Application {
         launch(args);
     }
 
-    public void start(Stage prim) {
+    @Override
+    public void start(Stage application) {
         Group root = new Group();
         Scene scene = new Scene(root, 500, 500, Color.LIGHTGRAY);
 
-        prim.setTitle("Windows Calendar");
-        prim.setScene(scene);
-
-
-        prim.show();
+        application.setTitle("Windows Calendar");
+        application.setScene(scene);
+        application.show();
     }
 }
